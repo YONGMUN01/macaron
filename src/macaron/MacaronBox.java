186678a1506112ff24e8eb4f3f3602db03d8c532
YONGMUN01 <rladyd887@gmail.com>
2025-06-26 15:13:05 +0900

    250626 클래스 틀 및 메인 틀

--- src/macaron/MacaronBox.java
+++ src/macaron/MacaronBox.java
@@ -2,4 +2,38 @@ package macaron;
 
 public class MacaronBox {
 
+	// 필드
+	String name;
+	int count;
+	int totlaPrice;
+	
+	String[] menu = {"값1", "값2", "값3", "값4", "값5"};
+	int[] price = {1, 10, 100, 1000, 10000};
+	String[] item = new String[100];
+	
+	
+	// order메소드 
+	void order(String name, int count, String item[]) {}
+	
+	
+	// print 메소드
+	void print() {
+	}
+	
+	
+	// addMacaron 메소드
+	void addMacaron() {
+		
+	}
+	
+	
+	// reset 메소드
+	void reset() {
+		
+	}
+	
+	
+	
+	
+	
 }
---
+++ src/macaron/MacaronMain.java
@@ -0,0 +1,52 @@
+package macaron;
+
+import java.util.Scanner;
+
+public class MacaronMain {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String name;
+		int count = 0;	// 현재 판매한 총 마카롱 개수
+		int totalSell = 100;	// 준비한 마카롱 개수
+		
+		String[] item = new String[100];
+		
+				
+		// 객체 생성
+		MacaronBox mc = new MacaronBox();
+		
+		while(totalSell > count) {
+			// 이름 입력
+			name = sc.nextLine();
+			System.out.print("몇 박스를 구매하시겠습니까? : ");
+			int num = sc.nextInt();		// 마카롱 주문 개수
+			
+			if(totalSell - count < num) {	// 마카롱개수가 남아있지 않을 때
+				System.out.println("그만한 수량이 남아있지 않습니다.");
+				continue;
+			} 
+			
+			for(int i = 0; i < num; i++) {
+				item[i] = sc.next();
+			}
+			sc.nextLine();
+			
+			
+			// 주문 메소드 호출
+			mc.order(name, num, item);
+			
+			// 초기화 메소드 호출
+			
+			
+			// 주문 출력
+			
+			
+			// 상자담기
+			
+		}
+		
+	}
+
+}
